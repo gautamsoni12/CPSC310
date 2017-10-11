d7e019ebd154ebd8b6a22bc32177fc38e6c4dc81
Gautam Soni <gtmsoni@yahoo.co.in>
2017-10-11 12:20:35 -0700

    Commit 6

--- src/controller/Courses.ts
+++ src/controller/Courses.ts
@@ -6,47 +6,54 @@ var request = require('request');
 var JSZip = require('jszip');
 var jsonfile = require('jsonfile');
 
-export class Course{
+export class Course {
 
-    //fileDest: string;
     id: string;
     content: string;
-    
+
     constructor(id: string, content: string) {
         this.id = id;
-        this.content= content;
-     }
+        this.content = content;
+    }
 
     createNewFile() {
-    let zip = new JSZip();
-    zip.folder('Courses').file('Hello');
+        let zip = new JSZip();
+        zip.folder('Courses').file('Hello');
 
     }
 
     // Parameter : takes a zip file
     // Returns : Array containing content of Zip file. 
 
-    loadfile(file: any) {
+    loadfile(file: any): Array<any> {
+        this.content = file;
         
-        var data:Array<any> = JSZip.loadAsync(file).then(function (zip:any) {
-            Object.keys(zip.files).forEach(function (zipfile) {
-              zip.files[zipfile].async('string').then(function (fileData:Array<any>) {
-                console.log(fileData) // These are your file contents      
-              })
-            })
-          })
-
-          return data;
+        try {
+            if (file != null) {
+                var data: Array<any> = JSZip.loadAsync(file).then(function (zip: any) {
+                    Object.keys(zip.files).forEach(function (zipfile) {
+                        zip.files[zipfile].async('string').then(function (fileData: Array<any>) {
+                            console.log(fileData) // These are file contents      
+                        })
+                    })
+                })
+            }
+            
+        } catch (emptyFileError) {
+            throw new Error(emptyFileError);
+        }
+        return data;
     }
 
     // Param : data from zip file
     // returns: Array of Json Object
 
-    convertToJson(jszipFile: any) {
-        var newFile:Array<any> = this.loadfile(jszipFile);
+    convertToJson(jszipFile: any): Array<any> {
+
+        var newFile: Array<any> = this.loadfile(jszipFile);
         var jsonArray: Array<any>;
 
-        for (let entry of newFile){
+        for (let entry of newFile) {
             var jsonObject = JSON.parse(JSON.stringify(entry));
             jsonArray.push(jsonObject);
         }
@@ -54,12 +61,10 @@ export class Course{
         return jsonArray;
     }
 
-    writeArrayToFile(){
-        var file = 'courses.json'
-        var object = this.convertToJson; 
-
+    writeArrayToFile(): void {
+        var file = this.id;
+        var object = this.convertToJson;
         jsonfile.writeFileSync(file, object);
     }
-
 
 }
--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,7 +1,7 @@
 /**
  * This is the main programmatic entry point for the project.
  */
-import {IInsightFacade, InsightResponse} from "./IInsightFacade";
+import { IInsightFacade, InsightResponse } from "./IInsightFacade";
 
 import Log from "../Util";
 
@@ -12,27 +12,61 @@ import { Course } from './Courses';
 var fs = require("fs");
 var request = require('request');
 var JSZip = require('jszip');
-
+var UBCInsight = new Map();
 export default class InsightFacade implements IInsightFacade {
 
     constructor() {
         Log.trace('InsightFacadeImpl::init()');
+
     }
+
 
     addDataset(id: string, content: string): Promise<InsightResponse> {
         let myCourse = new Course(id, content);
-        
-            return new Promise  (function (resolve, reject){
-        
-    
-            });
+        var zipContent: Array<any>;
+        var code: number;
+
+        return new Promise(function (resolve, reject) {
+            try {
+
+                zipContent = myCourse.loadfile(content);
+                zipContent = myCourse.convertToJson(zipContent);
+                if (UBCInsight.has(id)) {
+                    code = 201;
+                    resolve({ "code": code, "body": { res: 'the operation was successful and the id already existed' } });
+                } else {
+                    UBCInsight.set(id, zipContent);
+                    myCourse.writeArrayToFile();
+                    code = 204;
+                    resolve({ "code": code, "body": { res: 'the operation was successful and the id was new' } });
+                }
+            } catch (error) {
+                code = 400;
+                reject({ "code": code, "body": { res: error } })
+            }
+
+        });
     }
 
     removeDataset(id: string): Promise<InsightResponse> {
-        return null;
+        var code: number;
+        return new Promise(function (resolve, reject) {
+
+            try {
+                if (UBCInsight.has(id)){
+                    code = 204;
+                UBCInsight.delete(id);
+                resolve({ "code": code, "body": { res: 'the operation was successful' } })
+                }
+            } catch (error) {
+                code = 404;
+                reject({ "code": code, "body": { res: 'the operation was unsuccessful because the delete was for a resource that was not previously added.' } });
+            }
+        });
+
     }
 
-    performQuery(query: any): Promise <InsightResponse> {
+    performQuery(query: any): Promise<InsightResponse> {
         return null;
     }
 }
